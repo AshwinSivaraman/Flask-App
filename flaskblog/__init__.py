f89cf5b10a0cb9c4994ad975b5aabad8d15dbf7f
AshwinSivaraman <ashwinsr20@gmail.com>
2019-07-07 00:14:21 -0300

    added user account feature

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -8,6 +8,10 @@ app.config['SECRET_KEY'] = 'b09a0aacb8df24c6717e11a91f3d7814'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app )
-login_manager = LoginManager(app )
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+
 
 from flaskblog import routes
